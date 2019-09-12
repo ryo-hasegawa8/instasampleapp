9bffcb3fa697d6d7b0603758949f107e5d0d3ad7
EC2 Default User <ec2-user@ip-172-31-25-194.ap-southeast-1.compute.internal>
2019-09-12 07:22:04 +0000

    add env

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider:facebook, '467189170791489', 'a5891d0a8795010f850c7b1b2b68014b'
+  provider:facebook, ENV['467189170791489'], ENV['a5891d0a8795010f850c7b1b2b68014b']
 end
